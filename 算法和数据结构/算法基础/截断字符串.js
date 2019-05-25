a9816f29beb2f82fb231f538ee245b38038ead8b
Frank Liu <liuchunbo@gmail.com>
2019-05-25 15:31:59 +0800

    优化解题方法

--- 算法和数据结构/算法基础/截断字符串.js
+++ 算法和数据结构/算法基础/截断字符串.js
@@ -9,3 +9,32 @@ function truncateString(str, num) {
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+
+/*
+.cn的题略有不同，解法如下。
+
+截断字符串
+
+（用瑞兹来截断对面的退路）
+
+如果字符串的长度比指定的参数num长，则把多余的部分用...来表示。
+
+切记，插入到字符串尾部的三个点号也会计入字符串的长度。
+
+但是，如果指定的参数num小于或等于3，则添加的三个点号不会计入字符串的长度。
+
+当你完成不了挑战的时候，记得开大招'Read-Search-Ask'。
+
+这是一些对你有帮助的资源:
+
+String.slice()
+
+ */
+
+function truncate(str, num) {
+    // 请把你的代码写在这里
+    return num <=3 ? str.slice(0,num)+'...': str.length > num ? str.slice(0,num-3)+'...' : str;
+}
+
+truncate("A-tisket a-tasket A green and yellow basket", 11);
